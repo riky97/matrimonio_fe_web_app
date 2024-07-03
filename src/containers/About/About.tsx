7fdf74f2f32662e220f1ea8b4163d1fa707e8c57
riccardo.ingrasciotta <riccardo.ingrasciotta>
2024-07-03 16:50:20 +0200

    Aggiunta partecipanti

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -1,7 +1,7 @@
-import { Col, Image, Row } from "antd";
+import { Avatar, Col, Image, List, Row } from "antd";
 import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
-import { HouseDescriptionModel } from "../../utils/Models";
+import { HouseDescriptionModel, ParticipantModel } from "../../utils/Models";
 
 import "./About.css";
 import { ArrowLeftOutlined } from "@ant-design/icons";
@@ -11,6 +11,7 @@ interface IAboutProps { }
 function About(props: IAboutProps) {
     const location = useLocation();
     const houseFindedRef = useRef<HouseDescriptionModel>(location.state.houseFinded);
+    const userSearchedRef = useRef<string>(location.state.user);
 
     const containerImageRef = useRef<HTMLDivElement>(null);
     const containerDescriptionRef = useRef<HTMLDivElement>(null);
@@ -20,6 +21,9 @@ function About(props: IAboutProps) {
         // containerDescriptionRef.current.style.top = `${containerImageRef.current.offsetHeight - 20}px`;
     }, [])
 
+    // function orderToTopUserSearched(partecipants:  ParticipantModel[]) :  ParticipantModel[]{
+
+    // }
 
     return (
         <>
@@ -35,6 +39,23 @@ function About(props: IAboutProps) {
                         <h3 className="about_contaniner--title">{houseFindedRef.current.title}</h3>
 
                         <div className="about_contaniner--description">{houseFindedRef.current.description}</div>
+                    </Col>
+
+                    <Col xs={24} sm={0} className="mt-50">
+                        <h3 className="about_contaniner--title mb-10">Partecipanti</h3>
+
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={houseFindedRef.current.participants}
+                            renderItem={(item, index) => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<div style={{ display: "grid", placeItems: "center", width: 30, height: 30, borderRadius: "100%", background: "#186D1A", color: "#fff" }}>{index + 1}</div>}
+                                        title={item.name + " " + item.surname}
+                                    />
+                                </List.Item>
+                            )}
+                        />
                     </Col>
                 </Row>
             </section>
